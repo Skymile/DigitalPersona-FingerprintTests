f341fee09a045d0ccbc56844cad300e5041d033e
Patryk Milewski <mpatryk97@gmail.com>
2018-02-24 01:25:45 +0100

    UI Design

--- TestB/View/MainWindow.xaml.cs
+++ TestB/View/MainWindow.xaml.cs
@@ -1,58 +1,81 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows;
-
+using System.Windows.Input;
 using FDB.Biometrics;
 using FDB.Networking.Users;
 using FDB.ViewModel;
 
 namespace FDB.View
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-			WindowLabelMatching.Content = "Init";
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {   
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowLabelMatching.Content = "Init";
+        }
 
-		private void WindowButtonCapture_Click(object sender, RoutedEventArgs e) => mainModel.FingerprintCapture(ref WindowImage, ref WindowImageSource);
+        private void WindowButtonCapture_Click(object sender, RoutedEventArgs e) => mainModel.FingerprintCapture(ref WindowImage, ref WindowImageSource);
 
-		private void WindowButtonBinarize_Click(object sender, RoutedEventArgs e)
-		{
-			WindowImageSource = WindowImageSource.ApplyEffect(new[] {
-				-1, -1, -1,
-				-1,  8, -1,
-				-1, -1, -1
-			});
+        private void WindowButtonBinarize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowImageSource = WindowImageSource.ApplyEffect(new[] {
+                -1, -1, -1,
+                -1,  8, -1,
+                -1, -1, -1
+            });
 
-			WindowImage.Source = WindowImageSource.GetSource();
-		}
+            WindowImage.Source = WindowImageSource.GetSource();
+        }
 
-		private void WindowButtonRefresh_Click(object sender, RoutedEventArgs e) =>
-			WindowImage.Source = WindowImageSource.GetSource();
+        private void WindowButtonRefresh_Click(object sender, RoutedEventArgs e) =>
+            WindowImage.Source = WindowImageSource.GetSource();
 
-		private void WindowButtonIdentify_Click(object sender, RoutedEventArgs e) => mainModel.Verify(ref WindowLabelMatching);
+        private void WindowButtonIdentify_Click(object sender, RoutedEventArgs e) => mainModel.Verify(ref WindowLabelMatching);
 
-		private void WindowButtonSet_Click(object sender, RoutedEventArgs e) => mainModel.Set();
+        private void WindowButtonSet_Click(object sender, RoutedEventArgs e) => mainModel.Set();
 
-		private void WindowButtonListen_Click(object sender, RoutedEventArgs e) => mainModel.Listen(ref WindowLabelMatching);
+        private void WindowButtonListen_Click(object sender, RoutedEventArgs e) => mainModel.Listen(ref WindowLabelMatching);
 
-		private void WindowButtonLogin_Click(object sender, RoutedEventArgs e) => mainModel.Login();
+        private void WindowButtonLogin_Click(object sender, RoutedEventArgs e) => mainModel.Login();
 
-		private void WindowButtonRegister_Click(object sender, RoutedEventArgs e) => mainModel.Register();
+        private void WindowButtonRegister_Click(object sender, RoutedEventArgs e) => mainModel.Register(ref WindowLabelUsers);
+        
+        private User CurrentUser = User.Guest;
 
-		private User CurrentUser = User.Guest;
+        private Bitmap WindowImageSource = null;
+        private MainModel mainModel = new MainModel();
 
-		private Bitmap WindowImageSource = null;
-		private MainModel mainModel = new MainModel();
+        private void WindowSend_Click(object sender, RoutedEventArgs e)
+        {
+            SendWindow send = new SendWindow();
+            send.ShowDialog();
+        }
 
-		private void WindowSend_Click(object sender, RoutedEventArgs e)
-		{
-			SendWindow send = new SendWindow();
-			send.ShowDialog();
-		}
-	}
+        private void WindowExit_Click(object sender, RoutedEventArgs e) =>
+            Environment.Exit(0);
+
+        private void WindowMaximize_Click(object sender, RoutedEventArgs e) => 
+            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+        private void WindowMinimalize_Click(object sender, RoutedEventArgs e) => 
+            this.WindowState = WindowState.Minimized;
+
+        private void Menu_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed && Mouse.GetPosition(this).Y < 32)
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    Application.Current.MainWindow.Top = 3;
+                }
+                this.DragMove();
+            }
+        }
+    }
 }
--- TestB/ViewModel/MainModel.cs
+++ TestB/ViewModel/MainModel.cs
@@ -24,14 +24,15 @@ namespace FDB.ViewModel
 				this.scanner = new FingerprintScanner();
 			}
 			catch (TypeInitializationException)
-			{ // No scanner found, plug in device
-				throw new DeviceNotFoundException();
+			{
+				this.scanner = null;
+				new View.Message(View.Message.Error.DeviceNotFound);
 			}
 		}
 
 		public void FingerprintCapture(ref WindowControls.Image image, ref Bitmap imageBitmap)
 		{
-			imageBitmap = scanner.CaptureBitmap(0);
+			imageBitmap = this.scanner.CaptureBitmap(0);
 			image.Source = imageBitmap.GetSource();
 		}
 
@@ -41,25 +42,25 @@ namespace FDB.ViewModel
 		public void Listen(ref WindowControls.Label label) => this.server.ListenAsync(label);
 
 		public void Verify(ref WindowControls.Label label) => 
-			label.Content = (scanner.CaptureFingerprintData() == MainFingerprint).ToString();
+			label.Content = (this.scanner.CaptureFingerprintData() == this.MainFingerprint).ToString();
 
 		public void Login()
 		{
-			if (userbase.GetLength().GetValueOrDefault() != 0)
+			if (this.userbase.GetLength().GetValueOrDefault() != 0)
 			{
 				this.login = new View.LoginWindow(ref userbase, scanner);
-				login.ShowDialog();
+				this.login.ShowDialog();
 			}
 			else
 			{
-				message = new View.Message("You have to register first. " + userbase.GetLength().ToString());
-				message.ShowDialog();
+				this.message = new View.Message("You have to register first. " + this.userbase.GetLength().ToString());
+				this.message.ShowDialog();
 			}
 		}
 
-		public void Register()
+		public void Register(ref WindowControls.Label users)
 		{
-			this.register = new View.RegisterWindow(ref userbase, scanner);
+			this.register = new View.RegisterWindow(ref userbase, ref users, scanner);
 			this.register.ShowDialog();
 		}
 
